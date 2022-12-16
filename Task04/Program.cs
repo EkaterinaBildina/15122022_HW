6b3b7365055d0f9a2b30c48cd778a38addfa30de
Ekaterina Bildina <kukresch@gmail.com>
2022-12-16 13:26:35 +0300

    .gitignore updated (.vscode/ to add)

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,5 +1,34 @@
-﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
+﻿// Задача 4: Напишите программу, которая принимает 
+// на вход три числа 
+// и выдаёт максимальное из этих чисел.
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.Write("number1= ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("number2= ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("number3= ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (max > number2)
+{
+    max = number1;
+}
+else 
+{
+    max = number2;
+}
+
+if (max > number3)
+{
+    max = number2;
+}
+else 
+{
+    max = number3;
+}
+Console.WriteLine($"max = {max}");
